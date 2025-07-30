35a50869c403500d2e08eaa49fdd5616b90789be
Никита Бутенко <113593834+HeyItsNixee@users.noreply.github.com>
2025-07-30 14:36:13 +0300

    Removed FPS Counter

--- Assets/Scripts/UI_FPSCounter.cs
+++ Assets/Scripts/UI_FPSCounter.cs
@@ -3,15 +3,8 @@ using UnityEngine.UI;
 
 public class UI_FPSCounter : MonoBehaviour
 {
-    [SerializeField] private Text fpsText;
-
     private void Start()
     {
         Application.targetFrameRate = 60;
-    }
-
-    private void Update()
-    {
-        fpsText.text =  ((int)(1f / Time.unscaledDeltaTime)).ToString();
     }
 }
